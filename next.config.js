6830eb248e4da56548faa6e3e1530fed447fccf4
kamalsrini17 <159412754+kamalsrini17@users.noreply.github.com>
2025-06-16 16:24:42 -0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: {
+    appDir: true,
   },
-  images: { unoptimized: true },
-  trailingSlash: true,
-  distDir: 'out'
+  webpack: (config, { dev, isServer }) => {
+    // Disable webpack cache in development to prevent corruption
+    if (dev) {
+      config.cache = false;
+    }
+    
+    // Optimize build performance
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            priority: -10,
+            chunks: 'all',
+          },
+        },
+      },
+    };
+
+    return config;
+  },
+  // Disable SWC minification to prevent syntax errors
+  swcMinify: false,
+  // Enable strict mode
+  reactStrictMode: true,
+  // Disable source maps in production to reduce build size
+  productionBrowserSourceMaps: false,
 };
 
 module.exports = nextConfig;
